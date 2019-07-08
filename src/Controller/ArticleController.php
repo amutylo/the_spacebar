02ebc772b94f9191aac3a0501eab511b33c1df69
Andrii Mutylo <amutylo@webcodin.com>
2019-07-08 17:57:23 +0300

    added debug pack and unpack it

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,6 +29,8 @@ class ArticleController extends AbstractController
       'Wohoo! I\'m going on asteroid diet.',
       'I like bacon too! Buy some for me.'
     ];
+
+    dump($slug, $this);
     return $this->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
       'comments' => $comments
