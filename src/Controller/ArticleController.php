df51884e14bb3ced1f14df5011b9aa43737f865c
Andrii Mutylo <amutylo@webcodin.com>
2019-07-09 18:01:39 +0300

    added markdown

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use Michelf\MarkdownInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +25,7 @@ class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug)
+  public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
   {
     $comments = [
       'I ate normal rock once.',
@@ -31,9 +33,25 @@ class ArticleController extends AbstractController
       'I like bacon too! Buy some for me.'
     ];
 
-    dump($slug, $this);
+    $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](http://baconipsum.com/) aute enim veniam ut cillum **pork chuck** picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
+EOF;
+
+    $item = $cache->getItem('makrkdown' . md5($articleContent));
+
+    if (!$item->isHit()) {
+      $item->set($markdown->transform($articleContent));
+      $cache->save($item);
+    }
+    $articleContent = $item->get();
+
+    dump($markdown); die;
     return $this->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
+      'articleContent' => $articleContent,
       'comments' => $comments,
       'slug' => $slug
     ]);
